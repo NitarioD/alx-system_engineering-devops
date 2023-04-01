51bb0209f166f6e93b0d43dffc6f3fa4a552c984
Jola <nitariod@gmail.com>
2023-04-01 17:16:53 +0100

    0x0C 7 attempt 2

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Install Nginx web server (w/ Puppet)
 exec { 'server configuration':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello Worl    d!" | sudo tee /var/www/html/index.html; echo "http {\\n\\troot /var/www/html;\\n\\tindex index.html;\\n\\tserver {\\n\\t\\tlisten 80;\\n\\t\\tlocation /redirect_me {\\n\\t\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t\\t}\\n\\t}\\n}\\nevents {\\n}" | sudo tee /etc/nginx/nginx.conf;sudo service nginx restart',
 }
